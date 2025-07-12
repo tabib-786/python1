78dc32c8667a55b22b67f0827519f9231a3a178d
tabib-786 <aftabib76@gmail.com>
2025-07-12 22:06:03 +0530

    Add files via upload

---
+++ T1.py
@@ -0,0 +1,12 @@
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+add=num1+num2
+sub=num1 - num2
+mul=num1*num2
+div=num1%num2
+print("\n addition:",add)
+print("subtraction:",sub)
+print("multiplication:",mul)
+print("division:",div)
+
+
---
+++ T2.py
@@ -0,0 +1,4 @@
+string1=input("enter first name ")
+string2=input("enter second name ")
+string3= string1 +" "+ string2
+print("\nhello,"+string3+"! welcome to python program")
